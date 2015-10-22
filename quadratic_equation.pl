29778c060a3c7fd96d547136de22ace82e8f513e
Eduardo Aranda <eduardo.aranda@globant.com>
2015-10-22 17:35:26 -0300

    Validate input

--- quadratic_equation.pl
+++ quadratic_equation.pl
@@ -4,14 +4,26 @@
 #read input parameters
 my ($A,$B,$C) = @ARGV;
 
-#testing for absent arguments 
+#check for absent arguments 
 $A = ($A)?$A:0;
 $B = ($B)?$B:0;
 $C = ($C)?$C:0;
 
+#check for if only numbers
+for my $parameter ($A,$B,$C) {
+  if($parameter !~ /^-?(?:\d+\.?|\.\d)\d*\z/){
+   die ("bad input\n");
+  }
+}
+
  # using quadratic formula
+ if(($B ** 2 - 4 * $A * $C) < 0)
+ {  
+  die ("no real solutions\n");
+ }
+  
  my $x1 = (-$B + sqrt($B ** 2 - 4 * $A * $C)) / (2 * $A);
  my $x2 = (-$B - sqrt($B ** 2 - 4 * $A * $C)) / (2 * $A);
 
- say $x1;
- say $x2;
+ say "$x1 $x2";
+ 
