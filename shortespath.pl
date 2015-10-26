2664b5ba2decba292d808eaa7b0b0ae299894690
Eduardo Aranda <eduardo.aranda@globant.com>
2015-10-26 13:53:35 -0300

    Deal with cycles in the network

--- shortespath.pl
+++ shortespath.pl
@@ -8,8 +8,9 @@ use List::Util qw(min max);
 
 my %map;
 sub shortest_path {
-	my ($start,$end,$steps) = @_;
-
+	my ($start,$end,$steps,%visit_nodes) = @_;
+    #print "start: $start end: $end steps: $steps\n";
+    #print Dumper \%visit_nodes;
 	if($start eq $end){
 		return $steps;
 	}
@@ -17,12 +18,18 @@ sub shortest_path {
 		return -1;
 	}
 	my @distances;
-	foreach my $node (@{$map{$start}}){
-		my $current = shortest_path($node,$end,$steps+1);
-		push @distances,$current;
-	}
-	@distances = grep {$_ != -1} @distances;
-	my @sorted_distances = sort @distances;
+    
+    foreach my $node (@{$map{$start}}){
+        if(!exists $visit_nodes{$node} || $visit_nodes{$node} > ($steps+1)){
+            my $current = shortest_path($node,$end,$steps+1,%visit_nodes);
+            $visit_nodes{$node} = $current;
+            push @distances,$current;
+            #print Dumper \%visit_nodes;
+        }
+		
+    }
+	@distances = grep {$_ > 0 } @distances;
+    my @sorted_distances = sort @distances;
 	foreach my $dis (@sorted_distances){
 		if($dis != -1){
 			return $dis;
@@ -45,5 +52,8 @@ while(<points>){
 	}
 	push @map{$nodes[0]},$nodes[1];
 }
+#print Dumper \%map;
 my @point = split (" ",$input);
-print shortest_path($point[0],$point[1],0)."\n";
+my %visit_nodes;
+$visit_nodes{$point[0]} = 0;
+print shortest_path($point[0],$point[1],0,%visit_nodes)."\n";
